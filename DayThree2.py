5a1839d17673677140598e34ffcba8d892293f2c
Ana Carrillo <acarrillo9724@gmail.com>
2020-06-25 16:11:34 -0700

    work from day 3

---
+++ DayThree2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # the letter inside the brackets is being multiplied K number of times
+        # k > 0
+        # probably splice the string into idividual strings
+        # return the string decoded
+        
+        c = list()
+        for x in s:
+            c.append(x)
+            if i in x == '['
+            
+            
+        print(c)
